32af032f2b5fbd3264bc7eb4299fd1d92bbc29d3
emil648j <emilrubynielsen@gmail.com>
2021-01-28 13:12:54 +0100

    Fiksed

--- sketch.js
+++ sketch.js
@@ -20,19 +20,19 @@ function draw() {
         asteroids[i].update();
         asteroids[i].edges();
     }
+}
 
-    function keyReleased() {
-        ship.setRotation(0);
-        ship.boosting(false);
-    }
+function keyReleased() {
+    ship.setRotation(0);
+    ship.boosting(false);
+}
 
-    function keyPressed() {
-        if (keyCode == RIGHT_ARROW) {
-            ship.setRotation(0.1);
-        } else if (keyCode == LEFT_ARROW) {
-            ship.setRotation(-0.1);
-        } else if (keyCode == UP_ARROW) {
-            ship.boosting(true);
-        }
+function keyPressed() {
+    if (keyCode == RIGHT_ARROW) {
+        ship.setRotation(0.1);
+    } else if (keyCode == LEFT_ARROW) {
+        ship.setRotation(-0.1);
+    } else if (keyCode == UP_ARROW) {
+        ship.boosting(true);
     }
 }
